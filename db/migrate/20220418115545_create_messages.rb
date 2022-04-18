749cfcc798d187bfb75ff907db00c4d81ec919c1
ryota1119 <r.shinohara@wingdoor.co.jp>
2022-04-18 21:18:15 +0900

    バリデーションとUserモデルとのリレーションを設定

--- db/migrate/20220418115545_create_messages.rb
+++ db/migrate/20220418115545_create_messages.rb
@@ -2,6 +2,8 @@ class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
 
+      t.references :user
+      t.string :content
       t.timestamps
     end
   end
